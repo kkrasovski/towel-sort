d2105b607d888a85850e3959b7e412ea2502d9b1
Kiryl Krasouski <ziksow@gmail.com>
2021-02-26 22:53:13 +0300

    init: start tasl towel sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-
 // You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if (matrix) {
+    let outputArr = [];
+    let subArr;
+    for (let i = 0; i < matrix.length; i++) {
+      i % 2 != 0 ? subArr = matrix[i].reverse() : subArr = matrix[i];
+      for (let i = 0; i < subArr.length; i++) {
+        outputArr.push(subArr[i]);
+      }
+    }
+    return outputArr;
+  } else {
+    return outputArr = [];
+  }
 }
